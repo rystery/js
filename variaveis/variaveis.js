b8cb22632a3eca98ea1d5374f064c94e11480b3d
rystery <ryanzindrip123@gmail.com>
2026-03-19 21:59:33 -0300

    novascoisas

---
+++ variaveis/variaveis.js
@@ -0,0 +1,22 @@
+/*
+Variaveis:
+ Const (constante ou seja, se foi definido o valor ele nao muda mais)
+ let (variavel, ou seja, o valor pode ser alterado) 
+ var (descontinuado)
+*/
+
+const array = [{
+    nome: "Merdao",
+    idade: 20,
+    estadocivil: "Solteiro",
+    Premium: null
+},{nome: "Joao",
+    idade: 20,
+    estadocivil: "Solteiro",
+    Premium: true
+
+}]
+
+
+
+console.log(array)
